d564149cf764992663f3a987edc7b50fb093b8a1
sarainéscalderón <saraines.calderon@gmail.com>
2018-09-09 22:07:31 -0500

    copying from sarachicaddotcom

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pathPrefix: "/SaraChicaDDotCom",
+  siteMetadata: {
+    title: 'Sara Inés Calderón',
+  },
+  plugins: [
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-styled-components',
+  'gatsby-plugin-sass',
+  ],
+};
